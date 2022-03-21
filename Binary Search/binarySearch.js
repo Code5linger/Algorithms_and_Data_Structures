81376a0685c239638202034f37fa088e03edf2a5
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-03-21 20:59:59 +0600

    Iteration 2

--- Binary Search/binarySearch.js
+++ Binary Search/binarySearch.js
@@ -1,3 +1,4 @@
+/*
 const search = (val, arr) => {
   for (let i = 0; i < arr.length; i++) {
     if (val === arr[i]) {
@@ -8,5 +9,33 @@ const search = (val, arr) => {
 };
 
 const values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(search(7, values));
+*/
+/*
+const values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const search = (val, arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(i);
+    if (val === arr[i]) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+console.log("\n" + search(15, values) + " Output");
+*/
+const values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const search = (val, arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (val === arr[i]) {
+      return i;
+    }
+  }
+  return -404;
+};
 
 console.log(search(7, values));
