7147eb3f60c27f908b69a97fa1f4f46b26933eb1
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-03-21 21:08:08 +0600

    Ite 4

--- Binary Search/binarySearch.js
+++ Binary Search/binarySearch.js
@@ -41,6 +41,7 @@ const search = (val, arr) => {
 
 console.log(search(7, values));
 */
+/*
 const values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 let search = (val, arr) => {
@@ -52,3 +53,17 @@ let search = (val, arr) => {
   return -1;
 };
 console.log(search(100, values));
+*/
+const values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const valueNum2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const search = (val, arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (val === arr[i]) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+console.log(search(7, valueNum2));
