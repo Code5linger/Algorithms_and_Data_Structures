5ef16f5adc08b6c434c2446ab8380c33752e951b
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-03-21 21:13:40 +0600

    Giving Up For Today

--- Binary Search/binarySearch.js
+++ Binary Search/binarySearch.js
@@ -59,6 +59,7 @@ const valueNum2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const search = (val, arr) => {
   for (let i = 0; i < arr.length; i++) {
+    console.log(i);
     if (val === arr[i]) {
       return i;
     }
@@ -66,4 +67,21 @@ const search = (val, arr) => {
   return -1;
 };
 
-console.log(search(7, valueNum2));
+//  console.log(search(7, valueNum2));
+
+const binary = (val, arr) => {
+  let lower = 0;
+  let upper = arr.length - 1;
+
+  while (lower <= upper) {
+    const middle = lower.Math.floor(upper - lower) / 2;
+
+    if (val === arr[middle]) {
+      return middle;
+    }
+
+    if (val < arr[middle])
+  }
+
+  return -1;
+};
